e5aef8e86f8e47d391aa16533d3e5d72e96aedec
samiransamanta <31637538+samiransamanta@users.noreply.github.com>
2017-09-04 23:11:56 -0700

    Added new dot net core project
    New dot net core project Golden added

---
+++ Golden/TestLibrary/LibraryTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+namespace TestLibrary
+{
+    [TestClass]
+    public class LibraryTests
+    {
+        [TestMethod]
+        public void ThingGetsObjectValFromNumber()
+        {
+            Assert.AreEqual(42, new Thing().Get(42));
+        }
+    }
+}
